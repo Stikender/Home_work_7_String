b0c3e094f7dc30d39469569fe94165a5074ef545
Stikender <nadik17@bk.ru>
2022-11-16 18:40:41 +0800

    Finish exercise 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import javax.naming.InvalidNameException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Exercise 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        // Exercise 2
+        String fullNameUpperCase = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUpperCase);
+
+        // Exercise 3
+        String fullName2 = "Иванов Семён Семёнович";
+        String fullName2Correct = fullName2.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName2Correct);
     }
 }
